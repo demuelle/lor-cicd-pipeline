663da852c70be22913201826ba3db284b3eaf5af
demuelle <dmueller@trilogyed.com>
2023-09-13 13:39:11 -0500

    removes console log

--- src/components/CharacterGallery.jsx
+++ src/components/CharacterGallery.jsx
@@ -2,7 +2,6 @@ import Character from "./Character";
 import characters from "../data/characterData.json";
 
 const CharacterGallery = () => {
-    console.log(characters);
     return (
         <div>
             {characters.map(c => <Character key={c._id} {...c} />)}
